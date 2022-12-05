bde71855f3fcbd6f4872887b7fe3f774f92d0cac
Toros Kutlu <toros_kutlu@hotmail.com>
2022-12-05 09:53:46 +0100

    Long click edit reminder button placed, but needs functionality.
    Added updateReminder() in DataBaseHelper class.
    
    Working on design .xml of UpdateReminderActivity

---
+++ app/src/main/java/com/example/fcc_sqlite_demo/UpdateReminderActivity.java
@@ -0,0 +1,14 @@
+package com.example.fcc_sqlite_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_reminder);
+    }
+}
